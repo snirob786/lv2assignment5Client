44212e79bb004b56bca2f363a49e0210c22f78b7
Saifur Rahman Nirob <snirob786@gmail.com>
2024-04-06 21:56:19 +0600

    Update baseApi.ts

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://assignment-5-three-gamma.vercel.app/api/v1",
+  baseUrl: "https://assignment5backend-lovat.vercel.app/api/v1",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth.token;
